8c6e052ae584f5577d376dc7179eedcd1275fb2a
Muslim Yilmaz <mslmyilmaz5@gmail.com>
2023-05-05 21:23:16 +0300

    styling changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -4,14 +4,21 @@ $(document).ready(function(){
     
     var table = $('#example').DataTable({
         
-        buttons:['copy', 'csv', 'excel', 'pdf', 'print']
+        buttons:['copy', 'csv', 'excel', 'print']
         
     });
     
     
     table.buttons().container()
-    .appendTo('#example_wrapper .col-md-6:eq(0)');
-
+    .appendTo('#example_wrapper :eq(0)');
+    var $tableSearch = $('#tableSearch');
+    $('#example_filter').detach().appendTo($tableSearch);
+    $('#example_length').detach().prependTo($tableSearch);
+    $('<div>').css({
+      'display': 'flex',
+      'justify-content': 'flex-end' // Align items to the right
+    }).append($('#example_filter'), $('#example_length')).appendTo($tableSearch);
+     
 });
 
 // =============  Data Table - (End) ================= //
